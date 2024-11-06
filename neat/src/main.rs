44ad5fc5ab8f1eb7972cf9a719c35a4c9769861f
salt <eblack.leaf@proton.me>
2024-11-05 20:26:45 -0500

    feat:setup

--- neat/src/main.rs
+++ neat/src/main.rs
@@ -1,5 +1,5 @@
 use foliage::Foliage;
-const VIEW_AREA: (f32, f32) = (1800.0, 800.0);
+const VIEW_AREA: (f32, f32) = (1700.0, 800.0);
 fn main() {
     let mut foliage = Foliage::new();
     foliage.set_desktop_size(VIEW_AREA);
