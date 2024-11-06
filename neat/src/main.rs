aa332460d76e010cccefb7a41902a643656493fe
salt <eblack.leaf@proton.me>
2024-11-05 20:42:32 -0500

    feat:setup

--- neat/src/main.rs
+++ neat/src/main.rs
@@ -1,3 +1,24 @@
+pub(crate) mod snake;
+pub(crate) mod gene;
+pub(crate) mod genome;
+pub(crate) mod connection;
+pub(crate) mod node;
+pub(crate) mod food;
+pub(crate) mod game;
+pub(crate) mod input;
+pub(crate) mod output;
+pub(crate) mod mutation;
+pub(crate) mod selection;
+pub(crate) mod network;
+pub(crate) mod crossover;
+pub(crate) mod fitness;
+pub(crate) mod activation;
+pub(crate) mod bias;
+pub(crate) mod speciation;
+pub(crate) mod population;
+pub(crate) mod distance;
+mod neat;
+
 use foliage::Foliage;
 const VIEW_AREA: (f32, f32) = (1700.0, 800.0);
 fn main() {
