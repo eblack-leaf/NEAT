9dda66f746bd76d4b7357fdc0961193928886a9a
salt <eblack.leaf@proton.me>
2024-11-06 14:26:42 -0500

    feat:setup

--- neat/src/neat.rs
+++ neat/src/neat.rs
@@ -2,37 +2,38 @@ use rand::Rng;
 
 #[test]
 fn neat() {
-    // population (uniform) networks @ 2x1
-    let mut population = vec![Genome::new(2, 1); 150];
+    let mut population = vec![Genome::new(2, 1, -30.0, 30.0); 150];
     let generations = 300;
-    let fitness_threshold = 3.9;// if above this after fitness error calc => we end sim
+    let fitness_threshold = 3.9; // if above this after fitness error calc => we end sim
     let compatibility = Compatibility::new(1.0, 1.0, 0.5, 3.0);
     let perfect_fitness = 1.0 * XOR_INPUT.len() as f32;
-    // for g in generations
     for g in 0..generations {
-        //      next_gen = []
         let mut next_gen = vec![];
-        //      for p in population.len()
-        for genome in population.iter() {
-            let mut fitness = perfect_fitness;
+        for genome in population.iter_mut() {
+            genome.fitness = perfect_fitness;
             for (i, xi) in XOR_INPUT.iter().enumerate() {
                 let predicted = genome.activate(xi.to_vec());
                 let xo = XOR_OUTPUT[i];
-                fitness -= (predicted[0] - xo).powi(2);
+                genome.fitness -= (predicted[0] - xo).powi(2);
             }
+            // TODO normalize by species???
+            // need distance of each other in population * share-fn 0/1 modifier
         }
-        // selection of fittest
+        // let species_proportion = species-fitness-value (avg-of-genomes-in-species [after share-norm]) / population-total (all-species-totals);
+        // let species_offspring_num = species_proportion * population.len();
+        // selection of fittest (top 20% of total population)
         let mut selection = vec![];
+        // TODO modify below to account for how many offspring in each species (eases the species compat part)
         for _ in population.len() {
             // parent1 = random
             // parent2 = random (compatible w/ species of first)
             // crossover
             // mutate
-            // store in next_gen
+            // next_gen.push(mutated_crossover);
         }
-        //      population = next_gen
+        population = next_gen;
     }
-    // ans = population.max_fitness()
+    // ans = population.max_fitness() (iter to find best)
 }
 pub(crate) const XOR_INPUT: [[f32; 2]; 4] = [[1.0, 1.0], [1.0, 1.0], [1.0, 1.0], [1.0, 1.0]];
 pub(crate) const XOR_OUTPUT: [f32; 4] = [0.0, 1.0, 1.0, 0.0];
@@ -41,17 +42,19 @@ pub(crate) enum NodeType {
     Input,
     Hidden,
     Output,
+    Bias,
 }
 pub(crate) fn sigmoid(z: f32) -> f32 {
     1.0 / (1.0 + (-z).exp())
 }
 pub(crate) struct Node {
+    pub(crate) id: NodeId,
     pub(crate) value: f32,
     pub(crate) ty: NodeType,
 }
 impl Node {
-    pub(crate) fn new(ty: NodeType) -> Self {
-        Self { value: 0.0, ty }
+    pub(crate) fn new(id: NodeId, ty: NodeType) -> Self {
+        Self { id, value: 0.0, ty }
     }
 }
 pub(crate) struct Connection {
@@ -88,13 +91,13 @@ pub(crate) struct Genome {
     pub(crate) fitness: f32,
 }
 impl Genome {
-    pub(crate) fn new(inputs: usize, outputs: usize) -> Self {
+    pub(crate) fn new(inputs: usize, outputs: usize, weight_min: f32, weight_max: f32) -> Self {
         let mut nodes = vec![];
         for i in 0..inputs {
-            nodes.push(Node::new(NodeType::Input));
+            nodes.push(Node::new(i, NodeType::Input));
         }
         for o in 0..outputs {
-            nodes.push(Node::new(NodeType::Output));
+            nodes.push(Node::new(inputs + o, NodeType::Output));
         }
         let mut connections = vec![];
         for i in 0..inputs {
@@ -102,15 +105,25 @@ impl Genome {
                 connections.push(Connection::new(
                     i,
                     o,
-                    rand::thread_rng().gen_range(0.0..1.0),
+                    rand::thread_rng().gen_range(weight_min..weight_max),
                     true,
                 ));
             }
         }
-        Self { nodes, connections, fitness: 0.0 }
+        nodes.push(Node::new(nodes.len(), NodeType::Bias).value(1.0));
+        connections.push(Connection::new(
+            nodes.len().checked_sub(1).unwrap_or_default(),
+            inputs + 1,
+            rand::thread_rng().gen_range(weight_min..weight_max),
+            true,
+        ));
+        Self {
+            nodes,
+            connections,
+            fitness: 0.0,
+        }
     }
     pub(crate) fn activate(&self, inputs: Vec<f32>) -> Vec<f32> {
-        // add-bias term direct to output in evaluation
         todo!()
     }
 }
